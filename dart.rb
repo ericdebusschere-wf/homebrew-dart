8da682283232350c2caf928b0b58b519d35962a5
ericdebusschere-wf <ericdebusschere@wf14431.local>
2017-10-17 17:00:15 -0700

    ii

--- dart.rb
+++ dart.rb
@@ -39,9 +39,8 @@ class Dart < Formula
   end
 
   resource 'content_shell' do
-    version '1.17.0-dev.2.0'
-    url 'https://storage.googleapis.com/dart-archive/channels/dev/release/1.17.0-dev.2.0/dartium/content_shell-macos-ia32-release.zip'
-    sha256 '6e1522aae7e6772bf68f5d119d5e904a0687044a06913db06a3333088b3b523c'
+    version '1.24.2.0'
+    url 'http://gsdview.appspot.com/dart-archive/channels/stable/release/latest/dartium/dartium-macos-x64-release.zip'
   end
 
   def install
